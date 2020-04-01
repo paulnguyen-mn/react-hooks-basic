280c8cc316257ec5dd3a5ae544e1669e1f491d4c
Hau Nguyen <paulnguyen.mn@gmail.com>
2020-04-01 22:05:38 +0700

    Add color box lab

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.scss';
+import ColorBox from './components/ColorBox';
 
 function App() {
   return (
     <div className="app">
       <h1>Welcome to React Hooks!</h1>
+
+      <ColorBox />
     </div>
   );
 }

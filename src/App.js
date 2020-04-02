b1ecebc65c2455c3356b8ca4f12e0c205cd9cfee
Hau Nguyen <paulnguyen.mn@gmail.com>
2020-04-02 16:25:09 +0700

    useState lab - todo list - list and delete

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
-import ColorBox from './components/ColorBox';
+import TodoList from './components/TodoList';
 
 function App() {
+  const [todoList, setTodoList] = useState([
+    { id: 1, title: 'I love Easy Frontend! 😍' },
+    { id: 2, title: 'We love Easy Frontend! 🥰' },
+    { id: 3, title: 'They love Easy Frontend! 🚀' },
+  ]);
+
+  function handleTodoClick(todo) {
+    console.log(todo);
+    const index = todoList.findIndex(x => x.id === todo.id);
+    if (index < 0) return;
+
+    const newTodoList = [...todoList];
+    newTodoList.splice(index, 1);
+    setTodoList(newTodoList);
+  }
+
   return (
     <div className="app">
-      <h1>Welcome to React Hooks!</h1>
+      <h1>React hooks - TodoList</h1>
 
-      <ColorBox />
+      <TodoList todos={todoList} onTodoClick={handleTodoClick} />
     </div>
   );
 }
